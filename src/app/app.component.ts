7eb48bcc64656c9b67105e861c6a6904143e20b8
samuelsierra21 <samuelsierrabarrios9@gmail.com>
2024-02-22 17:11:28 -0500

    arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { HomeComponent } from './component/home/home.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { CategoriaComponent } from './component/categoria/categoria.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,HomeComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,HomeComponent,FooterComponent,FormularioComponent,
+  CategoriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { CategoriaComponent } from './component/categoria/categoria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'categoria', component:CategoriaComponent}
+    
+];
